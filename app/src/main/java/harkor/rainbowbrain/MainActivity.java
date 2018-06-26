f2ed00f5aeb280d7aeb2ea4024c4d083e6de7343
Harkor <krzbigos@gmail.com>
2018-06-26 23:17:29 +0200

    Initial commit

---
+++ app/src/main/java/harkor/rainbowbrain/MainActivity.java
@@ -0,0 +1,30 @@
+package harkor.rainbowbrain;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+    @OnClick(R.id.image_classic)
+    void classicClick(){
+        Intent intent=new Intent(this,ClassicActivity.class);
+        startActivity(intent);
+    }
+}
